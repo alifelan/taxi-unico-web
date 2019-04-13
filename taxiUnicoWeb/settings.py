a08941ff6603a505296dd91f56beafb302f43ed0
Ali <alifelanv@gmail.com>
2019-04-12 19:27:41 -0500

    Heroku settings

--- taxiUnicoWeb/settings.py
+++ taxiUnicoWeb/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
